5a7b2640ed1e8dea5da6b40045c39dc6f596f273
iskovskikh@gmail.com <Iskovskikh Alexey>
2021-11-04 12:10:35 +0300

    fix allowed_hostd

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pa$php1h5hwyfnntrb)_nl_x6z=8bab5$2fha$v5#@$_!^j10k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
