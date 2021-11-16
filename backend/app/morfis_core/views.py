128bdc3f7a59c2d1526cf407cbe8534a259aacda
Iskovskikh Alexey <iskovskikh@gmail.com>
2021-11-16 13:43:28 +0300

    Настроенна пажинация в ICD коды

--- backend/app/morfis_core/views.py
+++ backend/app/morfis_core/views.py
@@ -1,14 +1,26 @@
+import rest_framework.pagination
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets, permissions, generics, filters
+from rest_framework import viewsets, permissions, generics, filters, mixins
+from rest_framework.generics import GenericAPIView
 
 from morfis_core.morfis_models import case
 from .serializers import ICDcodeSerializer
 
 
+class MyDefaultPageNumberPagination(rest_framework.pagination.PageNumberPagination):
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 # class ICDcodeViewSet(viewsets.ModelViewSet):
-class ICDcodeViewSet(generics.ListCreateAPIView):
+# class ICDcodeViewSet(generics.ListCreateAPIView):
+class ICDcodeViewSet(generics.ListAPIView):
+# class ICDcodeViewSet(
+#     mixins.ListModelMixin,
+#     mixins.RetrieveModelMixin,
+#     GenericAPIView
+# ):
     """
     API endpoint that allows users to be viewed or edited.
     """
@@ -18,3 +30,4 @@ class ICDcodeViewSet(generics.ListCreateAPIView):
     queryset = case.ICDcode.objects.all()
     serializer_class = ICDcodeSerializer
     permission_classes = [permissions.AllowAny]
+    pagination_class = MyDefaultPageNumberPagination
