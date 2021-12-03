b438ace125c70fd2459d61eb5e512d576a54d4f6
Olivievs <mie2000@yandex.ru>
2021-12-03 19:00:00 +0300

    Добавил модель организаций

---
+++ backend/app/morfis_core/morfis_models/organization.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from morfis_core.morfis_models.address import Address
+from morfis_core.morfis_models.hospital import Hospital
+
+
+class Organization(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Название организации'
+    )
+
+
+    address = models.OneToOneField(
+        Address,
+        on_delete=models.CASCADE,
+        verbose_name='Адрес организации',
+        blank=True,
+        null=True,
+        default=None
+    )
+
+
+    def __str__(self):
+        return '%s' % self.title
